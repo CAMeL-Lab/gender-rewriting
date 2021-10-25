a44397a51a13419b4491647b5fa5089eb012c2c7
balhafni <ba63@nyu.edu>
2021-10-25 17:27:20 +0400

    M2 edits creation script

--- scripts/create_m2_edits.sh
+++ scripts/create_m2_edits.sh
@@ -5,94 +5,88 @@ conda activate python2
 export M2_SCORER=/home/ba63/gender-rewriting/m2scorer
 
 if [ "$1" = "v1.0" ]; then
-    export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-1.0/
-    export EDITS_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v1.0-edits
+    export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v1.0/norm_data/
+    export EDITS_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v1.0/edits
 
     printf "Creating M2 Edits for APGC v1.0:\n"
-    printf "\nCreating dev set M2 edits:\n\n"
-    # Dev set annotations
-    cat $DATA_DIR/D-set-dev.arin $DATA_DIR/D-set-dev.arin > $DATA_DIR/D-set-dev.arin+D-set-dev.arin
-    cat $DATA_DIR/D-set-dev.ar.M $DATA_DIR/D-set-dev.ar.F > $DATA_DIR/D-set-dev.ar.M+D-set-dev.ar.F
 
-    python  $M2_SCORER/scripts/edit_creator.py \
-    $DATA_DIR/D-set-dev.arin+D-set-dev.arin \
-    $DATA_DIR/D-set-dev.ar.M+D-set-dev.ar.F \
-    > $EDITS_DIR/dev.arin+dev.arin.to.dev.ar.M+dev.ar.F
+    for split in dev test
+    do
+        printf "\nCreating $split set M2 edits:\n\n"
+        cat $DATA_DIR/D-set-$split.arin.norm \
+        $DATA_DIR/D-set-$split.arin.norm \
+        > $DATA_DIR/D-set-$split.arin+D-set-$split.arin.norm
 
-    rm $DATA_DIR/D-set-dev.arin+D-set-dev.arin
-    rm $DATA_DIR/D-set-dev.ar.M+D-set-dev.ar.F
+        # combining the files together to create parallels
+        # arin+arin --> M+F
+        cat $DATA_DIR/D-set-$split.ar.M.norm \
+        $DATA_DIR/D-set-$split.ar.F.norm \
+        > $DATA_DIR/D-set-$split.ar.M+D-set-$split.ar.F.norm
 
-    printf "================================================="
-    printf "\nCreating test set M2 edits:\n\n"
+        # creating the edits
+        python  $M2_SCORER/scripts/edit_creator.py \
+        $DATA_DIR/D-set-$split.arin+D-set-$split.arin.norm \
+        $DATA_DIR/D-set-$split.ar.M+D-set-$split.ar.F.norm \
+        > $EDITS_DIR/$split.arin+$split.arin.to.$split.ar.M+$split.ar.F.norm
 
-    # Test set annotations
-    cat $DATA_DIR/D-set-test.arin \
-    $DATA_DIR/D-set-test.arin \
-    > $DATA_DIR/D-set-test.arin+D-set-test.arin
+        rm $DATA_DIR/D-set-$split.arin+D-set-$split.arin.norm
+        rm $DATA_DIR/D-set-$split.ar.M+D-set-$split.ar.F.norm
+        printf "=================================================\n"
+    done
 
-    cat $DATA_DIR/D-set-test.ar.M \
-    $DATA_DIR/D-set-test.ar.F \
-    > $DATA_DIR/D-set-test.ar.M+D-set-test.ar.F
-
-    python  $M2_SCORER/scripts/edit_creator.py \
-    $DATA_DIR/D-set-test.arin+D-set-test.arin \
-    $DATA_DIR/D-set-test.ar.M+D-set-test.ar.F \
-    > $EDITS_DIR/test.arin+test.arin.to.test.ar.M+test.ar.F
-
-    rm $DATA_DIR/D-set-test.arin+D-set-test.arin
-    rm $DATA_DIR/D-set-test.ar.M+D-set-test.ar.F
 
 elif [ "$1" = "v2.0" ]; then
-    export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-2.0/data
-    export EDITS_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v2.0-edits
+    export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v2.0/norm_data/
+    export EDITS_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v2.0/edits
 
     printf "Creating M2 Edits for APGC v2.0:\n"
-    printf "\nCreating dev set M2 edits:\n\n"
 
-    # Dev set annotations
-    cat $DATA_DIR/dev/dev.arin \
-    $DATA_DIR/dev/dev.arin \
-    $DATA_DIR/dev/dev.arin \
-    $DATA_DIR/dev/dev.arin \
-    > $DATA_DIR/dev/dev.arin+dev.arin+dev.arin+dev.arin
+    # m2 edits annotations
+    for split in dev test
+    do
+        printf "\nCreating $split set M2 edits:\n\n"
 
-    cat $DATA_DIR/dev/dev.ar.MM \
-    $DATA_DIR/dev/dev.ar.FM \
-    $DATA_DIR/dev/dev.ar.MF \
-    $DATA_DIR/dev/dev.ar.FF \
-    > $DATA_DIR/dev/dev.ar.MM+dev.ar.FM+dev.ar.MF+dev.ar.FF
+        # Taking out the <s></s> markers
+        cat $DATA_DIR/$split.arin.norm | sed 's/^<s>//g' | sed 's/<\/s>$//g' \
+        > $DATA_DIR/$split.arin.norm.no_marks
 
-    python  $M2_SCORER/scripts/edit_creator.py \
-    $DATA_DIR/dev/dev.arin+dev.arin+dev.arin+dev.arin \
-    $DATA_DIR/dev/dev.ar.MM+dev.ar.FM+dev.ar.MF+dev.ar.FF \
-    > $EDITS_DIR/dev.arin+dev.arin+dev.arin+dev.arin.to.dev.ar.MM+dev.ar.FM+dev.ar.MF+dev.ar.FF.edits
+        cat $DATA_DIR/$split.ar.MM.norm | sed 's/^<s>//g' | sed 's/<\/s>$//g' \
+        > $DATA_DIR/$split.ar.MM.norm.no_marks
 
-    rm $DATA_DIR/dev/dev.arin+dev.arin+dev.arin+dev.arin
-    rm $DATA_DIR/dev/dev.ar.MM+dev.ar.FM+dev.ar.MF+dev.ar.FF
+        cat $DATA_DIR/$split.ar.FM.norm | sed 's/^<s>//g' | sed 's/<\/s>$//g' \
+        > $DATA_DIR/$split.ar.FM.norm.no_marks
 
-    printf "================================================="
-    printf "\nCreating test set M2 edits:\n\n"
+        cat $DATA_DIR/$split.ar.MF.norm | sed 's/^<s>//g' | sed 's/<\/s>$//g' \
+        > $DATA_DIR/$split.ar.MF.norm.no_marks
 
-    # Test set annotations
-    cat $DATA_DIR/test/test.arin \
-    $DATA_DIR/test/test.arin \
-    $DATA_DIR/test/test.arin \
-    $DATA_DIR/test/test.arin \
-    > $DATA_DIR/test/test.arin+test.arin+test.arin+test.arin
+        cat $DATA_DIR/$split.ar.FF.norm | sed 's/^<s>//g' | sed 's/<\/s>$//g' \
+        > $DATA_DIR/$split.ar.FF.norm.no_marks
 
-    cat $DATA_DIR/test/test.ar.MM \
-    $DATA_DIR/test/test.ar.FM \
-    $DATA_DIR/test/test.ar.MF \
-    $DATA_DIR/test/test.ar.FF \
-    > $DATA_DIR/test/test.ar.MM+test.ar.FM+test.ar.MF+test.ar.FF
+        # combining the files together to create parallels
+        # arin+arin+arin+arin --> MM+FM+MF+FF
+        cat $DATA_DIR/$split.arin.norm.no_marks \
+        $DATA_DIR/$split.arin.norm.no_marks \
+        $DATA_DIR/$split.arin.norm.no_marks \
+        $DATA_DIR/$split.arin.norm.no_marks \
+        > $DATA_DIR/$split.arin+$split.arin+$split.arin+$split.arin.norm.no_marks
 
-    python  $M2_SCORER/scripts/edit_creator.py \
-    $DATA_DIR/test/test.arin+test.arin+test.arin+test.arin \
-    $DATA_DIR/test/test.ar.MM+test.ar.FM+test.ar.MF+test.ar.FF \
-    > $EDITS_DIR/test.arin+test.arin+test.arin+test.arin.to.test.ar.MM+test.ar.FM+test.ar.MF+test.ar.FF.edits
+        cat $DATA_DIR/$split.ar.MM.norm.no_marks \
+        $DATA_DIR/$split.ar.FM.norm.no_marks \
+        $DATA_DIR/$split.ar.MF.norm.no_marks\
+        $DATA_DIR/$split.ar.FF.norm.no_marks \
+        > $DATA_DIR/$split.ar.MM+$split.ar.FM+$split.ar.MF+$split.ar.FF.norm.no_marks
 
-    rm $DATA_DIR/test/test.arin+test.arin+test.arin+test.arin
-    rm $DATA_DIR/test/test.ar.MM+test.ar.FM+test.ar.MF+test.ar.FF
+        # creating the edits
+        python $M2_SCORER/scripts/edit_creator.py \
+        $DATA_DIR/$split.arin+$split.arin+$split.arin+$split.arin.norm.no_marks \
+        $DATA_DIR/$split.ar.MM+$split.ar.FM+$split.ar.MF+$split.ar.FF.norm.no_marks \
+        > $EDITS_DIR/$split.arin+$split.arin+$split.arin+$split.arin.to.$split.ar.MM+$split.ar.FM+$split.ar.MF+$split.ar.FF.norm
+
+
+        rm $DATA_DIR/$split.arin+$split.arin+$split.arin+$split.arin.norm.no_marks
+        rm $DATA_DIR/$split.ar.MM+$split.ar.FM+$split.ar.MF+$split.ar.FF.norm.no_marks
+        printf "=================================================\n"
+    done
 
 else
     printf "APGC version not found: Please choose v1.0 or v2.0!\n"

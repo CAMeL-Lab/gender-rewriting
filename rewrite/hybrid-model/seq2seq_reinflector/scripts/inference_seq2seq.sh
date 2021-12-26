9a9965612361268cceeb114cd6e0f01040319f20
balhafni <ba63@nyu.edu>
2021-12-26 11:20:12 +0400

    Model inference script

---
+++ rewrite/hybrid-model/seq2seq_reinflector/scripts/inference_seq2seq.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -p condo 
+# use gpus
+#SBATCH --gres=gpu:1
+# memory
+#SBATCH --mem=20GB
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+nvidia-smi
+module purge
+
+export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-2.0/data/new_token_data/
+# export DATA_DIR=/home/ba63/gender-reinflection/data/alhafni
+#  --first_person_only \
+#  --add_side_constraints \
+#  --embed_trg_gender \
+#  --trg_gender_embed_dim 10 \
+#  --embed_trg_gender \
+#  --trg_gender_embed_dim 10 \
+#  --use_morph_features 
+#  --analyzer_db_path /scratch/ba63/calima_databases/calima-msa/calima-msa-s31_0.4.2.utf8.db.copy-mod \
+# --embed_dim 128 \
+# --hidd_dim 256 \
+# --num_layers 2 \
+python main.py \
+ --data_dir $DATA_DIR \
+ --embed_dim 100 \
+ --add_side_constraints \
+ --hidd_dim 100 \
+ --num_layers 1 \
+ --learning_rate 5e-4 \
+ --seed 21 \
+ --model_path saved_models/joint.kann.pt \
+ --do_inference \
+ --inference_mode dev \
+ --beam_size 10 \
+ --n_best 5 \
+ --preds_dir logs/reinflection/dev.joint.kann

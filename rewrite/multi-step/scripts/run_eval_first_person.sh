872018316ea7687261e29037f557e357e2ef6a09
balhafni <ba63@nyu.edu>
2022-01-24 15:36:33 +0400

    M2 scorer and BLEU eval script for single user experiments

---
+++ rewrite/multi-step/scripts/run_eval_first_person.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH -p condo 
+# Set number of nodes to run
+#SBATCH --nodes=1
+# Set number of tasks to run
+#SBATCH --ntasks=1
+# Set number of cores per task (default is 1)
+#SBATCH --cpus-per-task=10 
+# memory
+#SBATCH --mem=10GB
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+export EXPERIMENT=CBR_filter+backoff+all+morph_newdb+mod_per_3rd_generator+neural_test
+export SYSTEM_HYP=logs/paper_results/first_person/reinflection/$EXPERIMENT
+
+# removing last empty line from the preds files
+sed -i '$ d' $SYSTEM_HYP/arin.to.M.preds
+sed -i '$ d' $SYSTEM_HYP/arin.to.F.preds
+
+# preparing the preds
+cat $SYSTEM_HYP/arin.to.M.preds $SYSTEM_HYP/arin.to.F.preds > $SYSTEM_HYP/$EXPERIMENT.inf
+
+# normalizing the preds
+python /home/ba63/gender-rewriting/rewrite/hybrid-model/utils/normalize.py --input_file $SYSTEM_HYP/$EXPERIMENT.inf --output_file $SYSTEM_HYP/$EXPERIMENT.inf.norm
+
+
+export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v1.0/
+export DATA_SPLIT=test
+export GOLD_DATA=norm_data/D-set-$DATA_SPLIT.ar.M+D-set-$DATA_SPLIT.ar.F.norm
+export EDITS_ANNOTATIONS=edits/$DATA_SPLIT.arin+$DATA_SPLIT.arin.to.$DATA_SPLIT.ar.M+$DATA_SPLIT.ar.F.norm
+export GOLD_ANNOTATION=$DATA_DIR/$EDITS_ANNOTATIONS
+export TRG_GOLD_DATA=$DATA_DIR/$GOLD_DATA
+
+
+eval "$(conda shell.bash hook)"
+conda activate python2
+
+m2_eval=$(python /home/ba63/gender-rewriting/m2scorer/m2scorer $SYSTEM_HYP/$EXPERIMENT.inf.norm $GOLD_ANNOTATION)
+
+conda activate python3
+
+# run accuracy evaluation
+accuracy=$(python /home/ba63/gender-rewriting/rewrite/joint/utils/metrics.py --trg_directory $TRG_GOLD_DATA --pred_directory $SYSTEM_HYP/$EXPERIMENT.inf.norm)
+
+# run BLEU evaluation
+bleu=$(sacrebleu $TRG_GOLD_DATA  -i $SYSTEM_HYP/$EXPERIMENT.inf.norm -m bleu --force)
+
+printf "%s\n%s\n%-12s%s" "$m2_eval" "$accuracy" "BLEU" ": $bleu" > eval.$EXPERIMENT
+

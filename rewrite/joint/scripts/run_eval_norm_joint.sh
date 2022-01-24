9a92c5acc270f52528ead7236da000a5286e7dc5
balhafni <ba63@nyu.edu>
2022-01-24 15:24:08 +0400

    Joint baselines eval script

--- rewrite/joint/scripts/run_eval_norm_joint.sh
+++ rewrite/joint/scripts/run_eval_norm_joint.sh
@@ -16,17 +16,12 @@
 
 module purge
 
-
-# export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v1.0/
 export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v2.0/
 export DATA_SPLIT=test
 export DECODING=inf
-# export GOLD_DATA=norm_data/D-set-$DATA_SPLIT.ar.M+D-set-$DATA_SPLIT.ar.F.norm
 export GOLD_DATA=norm_data/$DATA_SPLIT.ar.MM+$DATA_SPLIT.ar.FM+$DATA_SPLIT.ar.MF+$DATA_SPLIT.ar.FF.norm
-# export EDITS_ANNOTATIONS=edits/$DATA_SPLIT.arin+$DATA_SPLIT.arin.to.$DATA_SPLIT.ar.M+$DATA_SPLIT.ar.F.norm
 export EDITS_ANNOTATIONS=edits/$DATA_SPLIT.arin+$DATA_SPLIT.arin+$DATA_SPLIT.arin+$DATA_SPLIT.arin.to.$DATA_SPLIT.ar.MM+$DATA_SPLIT.ar.FM+$DATA_SPLIT.ar.MF+$DATA_SPLIT.ar.FF.norm
-# export SYSTEM_HYP=logs/reinflection/first_person_side_constraints/$DATA_SPLIT.joint+morph.inf.norm
-export SYSTEM_HYP=logs/reinflection/multi_user_side_constraints_newdb_checking/$DATA_SPLIT.joint+morph.$DECODING.norm
+export SYSTEM_HYP=logs/multi_user_side_constraints_newdb_clean_train/$DATA_SPLIT.joint.$DECODING.norm
 
 export GOLD_ANNOTATION=$DATA_DIR/$EDITS_ANNOTATIONS
 
@@ -46,4 +41,4 @@ accuracy=$(python utils/metrics.py --trg_directory $TRG_GOLD_DATA --pred_directo
 # run BLEU evaluation
 bleu=$(sacrebleu $TRG_GOLD_DATA  -i $SYSTEM_HYP -m bleu --force)
 
-printf "%s\n%s\n%-12s%s" "$m2_eval" "$accuracy" "BLEU" ": $bleu" > eval.multi_user_side_constraints_checking.$DECODING
+printf "%s\n%s\n%-12s%s" "$m2_eval" "$accuracy" "BLEU" ": $bleu" > eval.multi_user_newdb_clean_train.joint.$DECODING

d5c45d382cedbd8a427668555cd7223c0660d95a
balhafni <ba63@nyu.edu>
2022-01-26 12:57:27 +0400

    Gender rewriting bash script

--- rewrite/multi-step/scripts/run_rewriting.sh
+++ rewrite/multi-step/scripts/run_rewriting.sh
@@ -22,6 +22,7 @@ module purge
 # --use_cbr \
 # --cbr_ngram 2 \
 # --cbr_backoff \
+# --reduce_cbr_noise \
 # --pick_top_mle \
 # --use_rbr \
 # --rbr_top_tgt_rule \
@@ -31,23 +32,28 @@ module purge
 # /scratch/ba63/BERT_models/bert-base-arabic-camelbert-msa
 # /scratch/ba63/mlm_lm/bert-base-arabic-camelbert-msa-mlm
 
-# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc/dev_predictions.txt
 # /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/models/dev_predictions.txt
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc/dev_predictions.txt
 
-# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc/test_predictions.txt
 #/scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/models/test_predictions.txt
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc/test_predictions.txt
 
 # /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/single_user/models_acc/dev_predictions.txt
 # /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/single_user/models_acc/test_predictions.txt
 
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/models/google_MT/dev_predictions.txt
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc/google_MT/dev_predictions.txt
+
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/models/google_MT/test_predictions.txt
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc/google_MT/test_predictions.txt
+
 # /scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-1.0/new_token_data/
 
 python main.py \
---data_dir /scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-1.0/new_token_data/ \
+--data_dir /scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-2.0/data/new_token_data/ \
 --morph_db /scratch/ba63/calima_databases/calima-msa/calima-msa-s31_0.4.2.utf8.db.copy-mod \
 --bert_model /scratch/ba63/mlm_lm/bert-base-arabic-camelbert-msa-mlm \
---src_bert_tags_dir /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/single_user/models_acc/test_predictions.txt \
---first_person_only \
+--src_bert_tags_dir /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc/test_predictions.txt \
 --inference_mode test \
 --use_cbr \
 --cbr_ngram 2 \
@@ -55,13 +61,13 @@ python main.py \
 --reduce_cbr_noise \
 --use_morph \
 --use_seq2seq \
---seq2seq_model_path neural_rewriter/saved_models/single_user \
+--seq2seq_model_path neural_rewriter/saved_models/augmented_fix/ \
 --top_n_best 5 \
 --beam_width 10 \
 --use_gpu \
---output_dir logs/paper_results_with_mlm_ft_final/single_user/rewriting/CBR_MorphR_NeuralR_test \
+--output_dir logs/paper_results_with_mlm_ft_final/multi_user_with_clitics/augmentation/rewriting/CBR_MorphR_NeuralR_aug_id_aug_test \
 --analyze_errors \
---error_analysis_dir logs/paper_results_with_mlm_ft_final/single_user/error_analysis/CBR_MorphR_NeuralR_test
+--error_analysis_dir logs/paper_results_with_mlm_ft_final/multi_user_with_clitics/augmentation/error_analysis/CBR_MorphR_NeuralR_aug_id_aug_test
 
 # --output_dir /scratch/ba63/gender-rewriting/raw_openSub/augmentation/system_output/CBR_filter_2+backoff+all+morph_newdb+mod_per_3rd_generator+neural_fix_testing
 # --analyze_errors \

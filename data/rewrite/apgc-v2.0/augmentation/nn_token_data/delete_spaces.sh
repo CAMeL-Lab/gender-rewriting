1e7375dd77b60539efe9bb111a37f6c77221cbe9
balhafni <ba63@nyu.edu>
2022-04-19 15:23:03 +0400

    Scripts to prepare data

---
+++ data/rewrite/apgc-v2.0/augmentation/nn_token_data/delete_spaces.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -p nvidia -q nlp 
+# Set number of nodes to run
+#SBATCH --nodes=1
+# Set number of tasks to run
+#SBATCH --ntasks=1
+# Set number of cores per task (default is 1)
+#SBATCH --cpus-per-task=10 
+# memory
+#SBATCH --mem=10GB
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+python delete_spaces.py

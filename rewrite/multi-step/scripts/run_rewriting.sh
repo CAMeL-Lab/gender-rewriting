d36d610dca21f678f8a1b17870db9d922afda4f8
balhafni <ba63@nyu.edu>
2022-04-21 20:06:54 +0400

    Gender rewriting inference script

--- rewrite/multi-step/scripts/run_rewriting.sh
+++ rewrite/multi-step/scripts/run_rewriting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -p condo
+#SBATCH -p nvidia
 # use gpus
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:1
 # memory
 #SBATCH --mem=120000
 # Walltime format hh:mm:ss
@@ -13,53 +13,27 @@
 nvidia-smi
 module purge
 
-# --first_person_only \
-# --use_seq2seq \
-# --seq2seq_model_path neural_rewriter/saved_models \
-# --top_n_best 5 \
-# --beam_width 10 \
-# --use_morph \
-# --use_cbr \
-# --cbr_ngram 2 \
-# --cbr_backoff \
-# --reduce_cbr_noise \
-# --pick_top_mle \
-# --use_rbr \
-# --rbr_top_tgt_rule \
-# --rbr_top_rule
-
-
-# /scratch/ba63/BERT_models/bert-base-arabic-camelbert-msa
-# /scratch/ba63/mlm_lm/bert-base-arabic-camelbert-msa-mlm
-
-# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/models
-# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc
-
-# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/single_user/models_acc
-
-# /scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-1.0/new_token_data/
+# /home/ba63/gender-rewriting-camera-ready/data/rewrite/apgc-v1.0/
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/camera_ready/multi_user_with_clitics/models_f1/checkpoint-10000
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/camera_ready/single_user/models_f1/checkpoint-1000/
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/camera_ready/multi_user_with_clitics/augmented_models/5000/models_f1/checkpoint-55000
 
 python main.py \
---data_dir /scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-2.0/data/new_token_data/ \
+--data_dir /home/ba63/gender-rewriting/data/rewrite/apgc-v2.0/ \
 --morph_db /scratch/ba63/calima_databases/calima-msa/calima-msa-s31_0.4.2.utf8.db.copy-mod \
---gender_id_model /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/multi_user_with_clitics/controlled_settings/augmented_models_3_5000_acc \
---bert_model /scratch/ba63/mlm_lm/bert-base-arabic-camelbert-msa-mlm \
+--bert_model  /scratch/ba63/gender-rewriting/mlm_lm/bert-base-arabic-camelbert-msa-mlm-88 \
+--gender_id_model /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/camera_ready/multi_user_with_clitics/models_f1/checkpoint-10000 \
 --inference_mode dev \
 --use_cbr \
 --cbr_ngram 2 \
 --cbr_backoff \
 --reduce_cbr_noise \
---save_cbr_model \
 --use_morph \
 --use_seq2seq \
---seq2seq_model_path neural_rewriter/saved_models/augmented_fix/ \
---top_n_best 5 \
+--seq2seq_model_path neural_rewriter/saved_models/ \
+--top_n_best 3 \
 --beam_width 10 \
 --use_gpu \
---output_dir logs/paper_results_with_mlm_ft_final/multi_user_with_clitics/augmentation/rewriting/CBR_MorphR_NeuralR_aug_id_aug_checking \
+--output_dir /home/ba63/gender-rewriting/rewrite/multi-step/logs/multi_user/rewriting/CorpusR_MorphR_NeuralR
 --analyze_errors \
---error_analysis_dir logs/paper_results_with_mlm_ft_final/multi_user_with_clitics/augmentation/error_analysis/CBR_MorphR_NeuralR_aug_id_aug_checking
-
-# --output_dir /scratch/ba63/gender-rewriting/raw_openSub/augmentation/system_output/CBR_filter_2+backoff+all+morph_newdb+mod_per_3rd_generator+neural_fix_testing
-# --analyze_errors \
-# --error_analysis_dir /home/ba63/gender-rewriting/rewrite/hybrid-model/logs/multi_user_with_clitics_final/error_analysis/CBR_filter_2+backoff+all+morph_newdb+mod_per_3rd_generator+neural_fix
+--error_analysis_dir /home/ba63/gender-rewriting/rewrite/multi-step/logs/multi_user/error_analysis/CorpusR_MorphR_NeuralR

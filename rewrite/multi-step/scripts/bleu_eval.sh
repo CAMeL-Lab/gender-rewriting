f67fee26392842a41fb6e1d6a58e92ba0ab46a53
balhafni <ba63@nyu.edu>
2022-04-21 13:09:28 +0400

    MT post-editing bleu eval

--- rewrite/multi-step/scripts/bleu_eval.sh
+++ rewrite/multi-step/scripts/bleu_eval.sh
@@ -1,10 +1,10 @@
-EXPERIMENT=CBR_MorphR_NeuralR_aug_id_aug_test
-# EXPERIMENT=Google
-DATA_DIR=logs/paper_results_with_mlm_ft_final/multi_user_with_clitics/MT/$EXPERIMENT
-GOLD_DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/m2_edits/v2.0/norm_data/MT/
-split=test
+# EXPERIMENT=CorpusR_MorphR_NeuralR_aug_GID_aug_test
+EXPERIMENT=google_translate
+DATA_DIR=/home/ba63/gender-rewriting/rewrite/multi-step/logs/multi_user/MT/$EXPERIMENT
+GOLD_DATA_DIR=/home/ba63/gender-rewriting/data/m2_edits/v2.0/norm_data/MT
+split=dev
 
-if [ "$EXPERIMENT" = "Google" ]; then
+if [ "$EXPERIMENT" = "google_translate" ]; then
 
     for f in $GOLD_DATA_DIR/$split.ar.MM.norm $GOLD_DATA_DIR/$split.ar.FM.norm  $GOLD_DATA_DIR/$split.ar.MF.norm  $GOLD_DATA_DIR/$split.ar.FF.norm 
     do
@@ -13,26 +13,33 @@ if [ "$EXPERIMENT" = "Google" ]; then
     done
 
 else
-     sed -i '$ d' $DATA_DIR/arin.to.MM.preds
-     sed -i '$ d' $DATA_DIR/arin.to.FM.preds
-     sed -i '$ d' $DATA_DIR/arin.to.MF.preds
-     sed -i '$ d' $DATA_DIR/arin.to.FF.preds
 
-     python /home/ba63/gender-rewriting/rewrite/multi-step/utils/normalize.py --input_file $DATA_DIR/arin.to.MM.preds --output_file $DATA_DIR/arin.to.MM.preds.norm
-     python /home/ba63/gender-rewriting/rewrite/multi-step/utils/normalize.py --input_file $DATA_DIR/arin.to.FM.preds --output_file $DATA_DIR/arin.to.FM.preds.norm
-     python /home/ba63/gender-rewriting/rewrite/multi-step/utils/normalize.py --input_file $DATA_DIR/arin.to.MF.preds --output_file $DATA_DIR/arin.to.MF.preds.norm
-     python /home/ba63/gender-rewriting/rewrite/multi-step/utils/normalize.py --input_file $DATA_DIR/arin.to.FF.preds --output_file $DATA_DIR/arin.to.FF.preds.norm
+    python /home/ba63/gender-rewriting/rewrite/multi-step/utils/normalize.py \
+        --input_file $DATA_DIR/arin.to.MM.preds \
+        --output_file $DATA_DIR/arin.to.MM.preds.norm
 
-    printf "Evaluating  $DATA_DIR/arin.to.MM.preds.norm againts $GOLD_DATA_DIR/$split.ar.MM.norm:\n"
+     python /home/ba63/gender-rewriting/rewrite/multi-step/utils/normalize.py \
+        --input_file $DATA_DIR/arin.to.FM.preds \
+        --output_file $DATA_DIR/arin.to.FM.preds.norm
+
+     python /home/ba63/gender-rewriting/rewrite/multi-step/utils/normalize.py \
+        --input_file $DATA_DIR/arin.to.MF.preds \
+        --output_file $DATA_DIR/arin.to.MF.preds.norm
+
+     python /home/ba63/gender-rewriting/rewrite/multi-step/utils/normalize.py \
+        --input_file $DATA_DIR/arin.to.FF.preds \
+        --output_file $DATA_DIR/arin.to.FF.preds.norm
+
+    printf "Evaluating  $DATA_DIR/arin.to.MM.preds.norm against $GOLD_DATA_DIR/$split.ar.MM.norm:\n"
     cat $DATA_DIR/arin.to.MM.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.MM.norm --force
 
-    printf "Evaluating  $DATA_DIR/arin.to.FM.preds.norm againts $GOLD_DATA_DIR/$split.ar.FM.norm:\n"
+    printf "Evaluating  $DATA_DIR/arin.to.FM.preds.norm against $GOLD_DATA_DIR/$split.ar.FM.norm:\n"
     cat $DATA_DIR/arin.to.FM.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.FM.norm --force
 
-    printf "Evaluating  $DATA_DIR/arin.to.MF.preds.norm againts $GOLD_DATA_DIR/$split.ar.MF.norm:\n"
+    printf "Evaluating  $DATA_DIR/arin.to.MF.preds.norm against $GOLD_DATA_DIR/$split.ar.MF.norm:\n"
     cat $DATA_DIR/arin.to.MF.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.MF.norm --force
 
-    printf "Evaluating  $DATA_DIR/arin.to.FF.preds.norm againts $GOLD_DATA_DIR/$split.ar.FF.norm:\n"
+    printf "Evaluating  $DATA_DIR/arin.to.FF.preds.norm against $GOLD_DATA_DIR/$split.ar.FF.norm:\n"
     cat $DATA_DIR/arin.to.FF.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.FF.norm --force
 
 fi

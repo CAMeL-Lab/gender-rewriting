4daa227d0bb22014f4d9fa259bd40b8faa67d7b9
balhafni <ba63@nyu.edu>
2022-04-21 17:55:52 +0400

    Out-of-context neural rewriter training script

--- rewrite/multi-step/neural_rewriter/scripts/train_seq2seq.sh
+++ rewrite/multi-step/neural_rewriter/scripts/train_seq2seq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -p nvidia
+#SBATCH -p nvidia 
 # use gpus
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:1 
 # memory
 #SBATCH --mem=100GB
 # Walltime format hh:mm:ss
@@ -13,29 +13,13 @@
 nvidia-smi
 module purge
 
-# export DATA_DIR=/home/ba63/gender-reinflection/data/alhafni/
-#  --embed_trg_gender \
-#  --trg_gender_embed_dim 10 \
-#  --first_person_only \
-#  --use_morph_features \
-#  --analyzer_db_path /scratch/ba63/calima_databases/calima-msa/calima-msa-s31_0.4.2.utf8.db.copy-mod \
-#  --morph_features_path saved_models/morph_features_top_1_analyses.json \
-#  --add_side_constraints \
-#  --visualize_loss \
-#  --do_early_stopping \
-
-# export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-1.0/new_token_data/
-# export DATA_DIR=/scratch/ba63/Arabic-Parallel-Gender-Corpus/Arabic-parallel-gender-corpus-v-2.0/data/new_token_data/
-# export DATA_DIR=/scratch/ba63/gender-rewriting/raw_openSub/augmentation/augmented_data/CBR+backoff+all+morph_newdb+mod_per_3rd_generator+neural_mlm3_augmented
-# export DATA_DIR=/scratch/ba63/gender-rewriting/raw_openSub/augmentation/augmented_data/CBR+backoff+all+morph_newdb+mod_per_3rd_generator+neural_augmented
-export DATA_DIR=/scratch/ba63/gender-rewriting/raw_openSub/augmentation/augmented_data/CBR_filter_2+backoff+all+morph_newdb+mod_per_3rd_generator+neural_fix_augmented
-# export DATA_DIR=/scratch/ba63/gender-rewriting/raw_openSub/augmentation/augmented_data/CBR_filter+backoff+all+morph_newdb+mod_per_3rd_generator+neural_MLM_with_seed
+export DATA_DIR=/home/ba63/gender-rewriting/data/rewrite/apgc-v2.0/
+# export DATA_DIR=/home/ba63/gender-rewriting/data/rewrite/apgc-v2.0/augmentation/
 
 python main.py \
  --data_dir $DATA_DIR \
  --add_side_constraints \
- --analyzer_db_path /scratch/ba63/calima_databases/calima-msa/calima-msa-s31_0.4.2.utf8.db.copy-mod \
- --vectorizer_path saved_models/augmented_fix/vectorizer.json \
+ --vectorizer_path saved_models/multi_user/vectorizer.json \
  --cache_files \
  --num_train_epochs 50 \
  --embed_dim 128 \
@@ -49,4 +33,4 @@ python main.py \
  --dropout 0.2 \
  --clip_grad 1.0 \
  --do_early_stopping \
- --model_path saved_models/augmented_fix/joint.pt
+ --model_path saved_models/multi_user/joint.pt

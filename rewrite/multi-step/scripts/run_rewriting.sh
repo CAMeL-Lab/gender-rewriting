2af9a738abe98959c8e7ec8d9a890456fa8c49d7
balhafni <ba63@nyu.edu>
2022-05-03 01:38:59 +0400

    Gender rewriting inference script

--- rewrite/multi-step/scripts/run_rewriting.sh
+++ rewrite/multi-step/scripts/run_rewriting.sh
@@ -16,24 +16,24 @@ module purge
 # /home/ba63/gender-rewriting/data/rewrite/apgc-v1.0/
 # /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/camera_ready/multi_user_with_clitics/models_f1/checkpoint-10000
 # /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/camera_ready/single_user/models_f1/checkpoint-1000/
-# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/camera_ready/multi_user_with_clitics/augmented_models/5000/models_f1/checkpoint-55000
+# /scratch/ba63/gender-rewriting/gender-id/CAMeLBERT_MSA/camera_ready/multi_user_with_clitics/augmented_models/5000/models_f1_final/checkpoint-65000
 
 python main.py \
---data_dir /home/ba63/gender-rewriting/data/rewrite/apgc-v2.0/ \
+--data_dir /home/ba63/gender-rewriting/data/rewrite/apgc-v2.1 \
 --morph_db /scratch/ba63/gender-rewriting/models/calima-msa-s31_0.4.2.db \
---bert_model /scratch/ba63/gender-rewriting/models/bert-base-camel-bert-msa-apgcv2.0 \
---gender_id_model /scratch/ba63/gender-rewriting/models/gender-id-apgcv2.0 \
---inference_mode dev \
+--bert_model /scratch/ba63/gender-rewriting/models/bert-base-camel-bert-msa-apgcv2.1 \
+--gender_id_model /scratch/ba63/gender-rewriting/models/gender-id-apgcv2.1-aug \
+--inference_mode test \
 --use_cbr \
 --cbr_ngram 2 \
 --cbr_backoff \
 --reduce_cbr_noise \
 --use_morph \
 --use_seq2seq \
---seq2seq_model_path neural_rewriter/saved_models/multi_user \
+--seq2seq_model_path neural_rewriter/saved_models/multi_user_augmented \
 --top_n_best 3 \
 --beam_width 10 \
 --use_gpu \
---output_dir /home/ba63/gender-rewriting/rewrite/multi-step/logs/multi_user/rewriting/CorpusR_MorphR_NeuralR \
+--output_dir /home/ba63/gender-rewriting/rewrite/multi-step/logs/multi_user/augmentation/rewriting/CorpusR_MorphR_NeuralR_aug_GID_aug_test \
 --analyze_errors \
---error_analysis_dir /home/ba63/gender-rewriting/rewrite/multi-step/logs/multi_user/error_analysis/CorpusR_MorphR_NeuralR
+--error_analysis_dir /home/ba63/gender-rewriting/rewrite/multi-step/logs/multi_user/augmentation/error_analysis/CorpusR_MorphR_NeuralR_aug_GID_aug_test

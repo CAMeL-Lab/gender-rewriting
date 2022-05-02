dacac114036ecad9c227febebeb943c9cd21d3b7
balhafni <ba63@nyu.edu>
2022-05-03 01:38:03 +0400

    MT post-editing bleu eval

--- rewrite/multi-step/scripts/bleu_eval.sh
+++ rewrite/multi-step/scripts/bleu_eval.sh
@@ -1,15 +1,15 @@
-# EXPERIMENT=CorpusR_MorphR_NeuralR_aug_GID_aug_test
-EXPERIMENT=google_translate
+EXPERIMENT=CorpusR_MorphR_NeuralR_aug_GID_aug_test
+# EXPERIMENT=google_translate
 DATA_DIR=/home/ba63/gender-rewriting/rewrite/multi-step/logs/multi_user/MT/$EXPERIMENT
-GOLD_DATA_DIR=/home/ba63/gender-rewriting/data/m2_edits/v2.0/norm_data/MT
-split=dev
+GOLD_DATA_DIR=/home/ba63/gender-rewriting/data/m2_edits/v2.1/norm_data/MT
+split=test
 
 if [ "$EXPERIMENT" = "google_translate" ]; then
 
     for f in $GOLD_DATA_DIR/$split.ar.MM.norm $GOLD_DATA_DIR/$split.ar.FM.norm  $GOLD_DATA_DIR/$split.ar.MF.norm  $GOLD_DATA_DIR/$split.ar.FF.norm 
     do
     printf "Evaluation Against $f:\n"
-        cat $DATA_DIR/$split.google.ar.norm | sacrebleu $f --force
+        cat $DATA_DIR/$split.google.ar.norm | sacrebleu $f -w 2 --force
     done
 
 else
@@ -31,15 +31,15 @@ else
         --output_file $DATA_DIR/arin.to.FF.preds.norm
 
     printf "Evaluating  $DATA_DIR/arin.to.MM.preds.norm against $GOLD_DATA_DIR/$split.ar.MM.norm:\n"
-    cat $DATA_DIR/arin.to.MM.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.MM.norm --force
+    cat $DATA_DIR/arin.to.MM.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.MM.norm -w 2 --force
 
     printf "Evaluating  $DATA_DIR/arin.to.FM.preds.norm against $GOLD_DATA_DIR/$split.ar.FM.norm:\n"
-    cat $DATA_DIR/arin.to.FM.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.FM.norm --force
+    cat $DATA_DIR/arin.to.FM.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.FM.norm -w 2 --force
 
     printf "Evaluating  $DATA_DIR/arin.to.MF.preds.norm against $GOLD_DATA_DIR/$split.ar.MF.norm:\n"
-    cat $DATA_DIR/arin.to.MF.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.MF.norm --force
+    cat $DATA_DIR/arin.to.MF.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.MF.norm -w 2 --force
 
     printf "Evaluating  $DATA_DIR/arin.to.FF.preds.norm against $GOLD_DATA_DIR/$split.ar.FF.norm:\n"
-    cat $DATA_DIR/arin.to.FF.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.FF.norm --force
+    cat $DATA_DIR/arin.to.FF.preds.norm | sacrebleu $GOLD_DATA_DIR/$split.ar.FF.norm -w 2 --force
 
 fi
